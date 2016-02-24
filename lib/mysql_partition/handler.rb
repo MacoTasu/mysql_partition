40b7e9f99ce34afb0ec8cf0f0eca1fd93a6bf40a
MacoTasu <maco.tasu@gmail.com>
2016-02-24 11:47:41 +0900

    実行できるメソッドを制限する。sql_makerのクラスとみつになってしまう点が難点

--- lib/mysql_partition/handler.rb
+++ lib/mysql_partition/handler.rb
@@ -2,7 +2,7 @@ require "mysql_partition/sql_maker"
 
 module MysqlPartition
   class Handler
-    attr_accessor :dbh, :executed, :sql_maker
+    attr_accessor :dbh, :sql_maker
     def initialize(hash)
       # dbh = Sequel only
       if !(hash[:dbh])
@@ -11,17 +11,18 @@ module MysqlPartition
 
       @sql_maker = SqlMaker.new(hash)
       @dbh = hash[:dbh]
-      @executed = 0
-      @hash = hash
+    end
+
+    def method_list
+      %{create_partitions drop_partiitons add_partitions}
     end
 
     def method_missing name, *args, &c
-      if (@sql_maker.respond_to?(name))
+      if (@sql_maker.respond_to?(name) && self.method_list.include?(name))
         statement = @sql_maker.send(name, *args)
-        p statement
         @dbh.run(statement)
       else
-        raise StandardError, "not implement #{name} in sql maker"
+        raise StandardError, "#{name} is not implemented in sql maker, or disallowed method"
       end
     end
   end
